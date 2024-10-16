fac32289a262ea94ffa85c1cf93278ec0d3438d8
Cov001 <155533946+Cov001@users.noreply.github.com>
2024-10-16 15:08:14 +0700

    Update SignIn.jsx

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -9,12 +9,15 @@ const SignIn = () => {
   const [pass, setPass] = useState("");
   const { state, dispatch } = useContext(AppContext);
   const navigate = useNavigate();
+  console.log(import.meta.env.VITE_API_NODE_ENV === "production"
+      ? import.meta.env.VITE_API_PRODUCTION
+      : import.meta.env.VITE_API_DEVELOPMENT;);
   const url =
     import.meta.env.VITE_API_NODE_ENV === "production"
       ? import.meta.env.VITE_API_PRODUCTION
       : import.meta.env.VITE_API_DEVELOPMENT;
 
-  console.log(url);
+  
   const handleSignIn = async () => {
     if (email === "" || pass === "") {
       toast.warning("Please fill all fields!");
